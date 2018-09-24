4cb7c912ccb130da7b5f4af7d99e4188e6a63393
Padmanabh Gupta <padmanabh14@gmail.com>
2018-09-25 01:02:26 +0530

    Initialized and added input layer ANN

--- NeuralNet.py
+++ NeuralNet.py
@@ -25,7 +25,7 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:,1:]
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -33,3 +33,13 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+
+#ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+no_units_hidden_layer = 6 
+classifier = Sequential()
+classifier.add(Dense(no_units_hidden_layer,init='uniform',activation='relu'))
